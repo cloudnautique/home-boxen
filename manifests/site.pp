43d875c7e041aa6e5564de942176c8ad7f3efa9b
William Maxwell <wmaxwell@williams-imac.local>
2013-06-23 21:25:17 -0700

    arg... need to look at it more

--- manifests/site.pp
+++ manifests/site.pp
@@ -78,13 +78,6 @@ node default {
   include ruby::1_9_3
   include ruby::2_0_0
 
-  # Include rbenv plugin
-  include ruby
-  ruby::plugin {'rbenv-gemset':
-    ensure => 'v0.3.0',
-    source => 'jamis/rbenv-gemset'
-  }
-
   # common, useful packages
   package {
     [
