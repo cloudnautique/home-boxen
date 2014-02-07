e2eea41f01ddf69c30356f38db5a6e7ad4928e45
Bill Maxwell <bill.t.maxwell@gmail.com>
2014-02-06 21:39:42 -0700

    updated

--- modules/people/manifests/cloudnautique.pp
+++ modules/people/manifests/cloudnautique.pp
@@ -1,12 +1,13 @@
 class people::cloudnautique {
   include textmate::textmate2::release
+  include redis
 
   class {'ruby::global':
     version => '1.9.3'
   }
 
   class {'packer':
-    version => '0.4.0'
+    version => '0.5.1'
   }
 
 }
