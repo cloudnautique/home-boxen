9257ca42bb396e8688587ae9a8139b09ad7b9f47
Bill Maxwell <bmaxwell@local-motors.com>
2014-04-16 15:51:45 -0700

    updated for new mac

--- modules/people/manifests/cloudnautique.pp
+++ modules/people/manifests/cloudnautique.pp
@@ -1,13 +1,15 @@
 class people::cloudnautique {
   include textmate::textmate2::release
   include redis
-
-  class {'ruby::global':
-    version => '1.9.3'
-  }
+  include chrome
+  include slack
 
   class {'packer':
-    version => '0.5.1'
+    version => '0.5.2'
   }
 
+
+  class { 'nodejs::global':
+    version => 'v0.10.26'
+  }
 }
