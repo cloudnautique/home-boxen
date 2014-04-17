62a7ec2252769732d711098b2494e74d7a586a37
Bill Maxwell <bill.t.maxwell@gmail.com>
2014-04-16 17:50:01 -0700

    updating

--- modules/people/manifests/cloudnautique.pp
+++ modules/people/manifests/cloudnautique.pp
@@ -1,6 +1,10 @@
 class people::cloudnautique {
   include textmate::textmate2::release
   include redis
+  
+  class {'java':
+    update_version => '51'
+  }
 
   class {'packer':
     version => '0.5.1'
